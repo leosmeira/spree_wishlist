66b7d565dbfca68d5731cb0844eed8989b6a3a67
leosmeira <leosmeira@gmail.com>
2016-09-29 16:02:34 -0300

    fix

--- app/controllers/spree/wishlists_controller.rb
+++ app/controllers/spree/wishlists_controller.rb
@@ -11,7 +11,7 @@ class Spree::WishlistsController < Spree::BaseController
   end
 
   def index
-    @wishlists = current_user.wishlists
+    @wishlists = current_user.try(:wishlists)
 
     respond_with(@wishlist)
   end
